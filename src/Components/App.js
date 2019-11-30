477bec7feb6680ca238a806b4ed6f9de7d94f04d
Mariel-local <mariel@hivesystems.cl>
2019-11-30 13:33:39 -0300

    landing estilos

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faUsers, faUser, faBookOpen, faLock, faBell, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faUsers, faUser, faBookOpen, faLock, faBell, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Landing from './Landing/Landing';
 import Main from './Main/Main';
 import './App.css';
 
-library.add(faUsers, faUser, faBookOpen, faLock, faBell, faChevronLeft);
+library.add(faUsers, faUser, faBookOpen, faLock, faBell, faChevronLeft,  faChevronRight);
 
 class App extends Component {
   render() {
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import './landing.css';
 import Carousel from 'nuka-carousel';
+import Img1 from '../../img/1.png';
+import Img2 from '../../img/2.png';
+import Img3 from '../../img/3.png';
 
 class Landing extends Component {
 	
@@ -14,10 +18,27 @@ class Landing extends Component {
 	render() {
 		return(
 			<div className="body">
-				<Carousel className="CarrouselContainer">
-					<img src="http://placehold.it/1000x400/ffffff/c0392b/&text=info1" alt="info de introduccion"/>
-					<img src="http://placehold.it/1000x400/ffffff/c0392b/&text=info2" alt="info de introduccion"/>
-					<img src="http://placehold.it/1000x400/ffffff/c0392b/&text=info3" alt="info de introduccion"/>
+				<Carousel className="CarrouselContainer m-auto" renderCenterLeftControls={({ previousSlide }) => (
+						<button onClick={previousSlide} className="slideBtn"><FontAwesomeIcon className="nav-icons flecha" icon="chevron-left"/></button>
+					)}
+					renderCenterRightControls={({ nextSlide }) => (
+						<button onClick={nextSlide} className="slideBtn"><FontAwesomeIcon className="nav-icons flecha" icon="chevron-right"/></button>
+					)}>
+					<div className="slideCard">
+						<img className="imgLanding" src={Img1} alt="info de introduccion"/>
+						<p className="title">¿Qué es Sana Tus Finanzas?</p>
+						<p className="text">Es una guía hacia una buena salud financiera, entregando herramientas para lograr metas de vivienda, salud y educación.	</p>
+					</div>		
+					<div className="slideCard">
+						<img className="imgLanding" src={Img2} alt="info de introduccion"/>
+						<p className="title">Pilares de Sana Tus Finanzas</p>
+						<p className="text">Es una guía hacia una buena salud financiera, entregando herramientas para lograr metas de vivienda, salud y educación.	</p>
+					</div>		
+					<div className="slideCard">
+						<img className="imgLanding" src={Img3} alt="info de introduccion"/>
+						<p className="title">Beneficios de Sana tus Finanzas</p>
+						<p className="text">Es una guía hacia una buena salud financiera, entregando herramientas para lograr metas de vivienda, salud y educación.	</p>
+					</div>		
       	</Carousel>
 				<Link className="landing-button" to="/entrar">Comenzar el test</Link>
 			</div>
